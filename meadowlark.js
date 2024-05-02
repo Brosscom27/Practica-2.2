e9989ede2bc116963598e69aa2c4ad5cd9ca6ab2
Brosscom27 <mcordovacsx@gmail.com>
2024-05-02 00:17:05 -0600

    creacion del archivo principal y las vistas

---
+++ meadowlark.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import {engine} from 'express-handlebars';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000 
+
+app.engine('handlebars', engine({
+        defaultLayout: 'main',
+}));
+
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.use((req, res) =>{
+    res.status(404);
+    res.render('404');
+});
+
+app.use((req, res) =>{
+    res.status(5000);
+    res.render('500');
+});
+
+
+app.listen(port, () =>{
+    console.log("listening successful");
+}); 
